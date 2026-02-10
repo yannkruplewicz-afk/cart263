96c4bc7c0584b8dd80dbb242deeeae68f925d9cc
yannkruplewicz-afk <yann.kruplewicz@gmail.com>
2026-02-09 20:24:55 -0500

    CART 263 TASK 3 FINAL

---
+++ task-3/task-3/task-3-js/team-a.js
@@ -0,0 +1,147 @@
+setup_A();
+/** THEME: CALM  */
+function setup_A() {
+  console.log("in a");
+  /**************************************************** */
+  //get the buttons
+  activateButtons(`#TEAM_A`, "ani_canvA", aniA, aniB, aniC, aniD);
+  /**************** ANI A ************************************ */
+  /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN A INSIDE HERE */
+  /**************** ANI A ************************************ */
+  /**************** TASK *******************************************
+   * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:)
+   * 1: create a creative, visual pattern using text, divs as shapes, images ... 
+   * 2: add in mouseclick event listener(s) somewhere to make the sketch interactive
+   *
+   * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
+   * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
+   * this is so that your styles are not overriden by other teams.
+   * NOTE::: All your code is to be added here inside this function  -
+   * remember you can define other functions inside....
+   * Do not change any code above or the HTML markup.
+   * **/
+
+  function aniA(parentCanvas) {
+    console.log("in ani-A -teamA");
+  }
+
+
+  /****************ANI B ************************************ */
+  /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN B INSIDE HERE */
+  /****************ANI B ************************************ */
+  /**************** TASK *******************************************
+   * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:).
+   * 1: create a creatve, visual pattern using text, divs as shapes, images ... 
+   * 2: add in mouseover event listener(s) somewhere to make the sketch interactive
+   *
+   * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
+   * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
+   * this is so that your styles are not overriden by other teams.
+   * NOTE::: All your code is to be added here inside this function -
+   * remember you can define other functions inside....
+   * Do not change any code above or the HTML markup.
+   * **/
+
+  function aniB(parentCanvas) {
+    console.log("in ani-B -teamA");
+
+    // make sure stamps position correctly inside this canvas
+    // parentCanvas.style.position = "relative";
+    // parentCanvas.style.overflow = "hidden";
+
+    // mousemove = paint
+    parentCanvas.addEventListener("mousemove", paintGlow);
+
+    function paintGlow(e) {
+      // "this" is the parentCanvas because the event listener is attached to it
+      const rect = this.getBoundingClientRect();
+
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      const stamp = document.createElement("div");
+      stamp.classList.add("TEAM_A_glowStamp");
+
+      // position the stamp
+      stamp.style.left = x + "px";
+      stamp.style.top = y + "px";
+
+      // random color
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      stamp.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+      // stamp.style.boxShadow = `0 0 12px rgb(${r}, ${g}, ${b})`;
+
+      this.appendChild(stamp);
+
+      // // fade out, then remove
+      // setTimeout(() => {
+      //   stamp.style.opacity = "0";
+      // }, 10);
+
+      // setTimeout(() => {
+      //   stamp.remove();
+      // }, 800);
+    }
+
+  }
+  /****************ANI C ************************************ */
+  /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN C INSIDE HERE */
+  /****************ANI C************************************ */
+  /**************** TASK *******************************************
+   * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:)
+   * 1: use the PROVIDED keyup/down callbacks `windowKeyDownRef` and/or `windowKeyUpnRef` to handle keyboard events
+   * 2: create an interactive pattern/sketch based on keyboard input. Anything goes.
+   * 
+   * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
+   * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
+   * this is so that your styles are not overriden by other teams.
+   * NOTE::: All your code is to be added here inside this function -
+   * remember you can define other functions inside....
+   * Do not change any code above or the HTML markup.
+   * **/
+
+  /* TASK: make an interactive pattern .. colors, shapes, sizes, text, images....
+   * using  ONLY key down and/or keyup -- any keys::
+   */
+
+  function aniC(parentCanvas) {
+    console.log("in ani-C -teamA");
+
+    /*** THIS IS THE CALLBACK FOR KEY DOWN (* DO NOT CHANGE THE NAME *..) */
+    windowKeyDownRef = function (e) {
+      //code for key down in here
+      console.log(e);
+      console.log("a-down");
+    };
+
+    /*** THIS IS THE CALLBACK FOR KEY UP (*DO NOT CHANGE THE NAME..) */
+    windowKeyUpRef = function (e) {
+      console.log("a-up");
+      console.log(e);
+    };
+    //DO NOT REMOVE
+    window.addEventListener("keydown", windowKeyDownRef);
+    window.addEventListener("keyup", windowKeyUpRef);
+  }
+
+  /****************ANI D************************************ */
+  /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN D INSIDE HERE */
+  /****************ANI D************************************ */
+  /**************** TASK *******************************************
+   * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:).
+   * 1: create a creative, visual pattern using text, divs as shapes, images ...
+   * 2: add in animation using requestAnimationFrame somewhere to make the sketch animate :)
+   *
+   * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
+   * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
+   * this is so that your styles are not overriden by other teams.
+   * NOTE::: All your code is to be added here inside this function -
+   * remember you can define other functions inside....
+   * Do not change any code above or the HTML markup.
+   * **/
+  function aniD(parentCanvas) {
+    console.log("in ani-D -teamA");
+  }
+}
---
+++ task-3/task-3/task-3-js/team-f.js
@@ -0,0 +1,181 @@
+setup_F();
+/** THEME: JOY  */
+function setup_F() {
+  console.log("in f");
+  /**************************************************** */
+  //get the buttons
+  activateButtons(`#TEAM_F`, "ani_canvF", aniA, aniB, aniC, aniD);
+
+  /**************** ANI A ************************************ */
+  /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN A INSIDE HERE */
+  /**************** ANI A ************************************ */
+  /**************** TASK *******************************************
+   * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:)
+   * 1: create a creative, visual pattern using text, divs as shapes, images ...
+   * 2: add in mouseclick event listener(s) somewhere to make the sketch interactive
+   *
+   * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
+   * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
+   * this is so that your styles are not overriden by other teams.
+   * NOTE::: All your code is to be added here inside this function  -
+   * remember you can define other functions inside....
+   * Do not change any code above or the HTML markup.
+   * **/
+
+  function aniA(parentCanvas) {
+    console.log("in ani-A -teamF");
+    const imgUrls = [];
+    for (let i = 0; i < 10; i++) {
+      imgUrls.push(`assets/${i}.jpg`)
+    }
+    let newElement = document.createElement('img');
+    newElement.src = imgUrls[1];
+    newElement.classList.add('TEAM_F_joyImg');
+    parentCanvas.appendChild(newElement);
+    parentCanvas.addEventListener('click', changeImgHandler);
+
+    function changeImgHandler() {
+      newElement.src = imgUrls[Math.floor(Math.random() * 10)];
+    }
+  }
+
+
+  /****************ANI B ************************************ */
+  /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN B INSIDE HERE */
+  /****************ANI B ************************************ */
+  /**************** TASK *******************************************
+   * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:).
+   * 1: create a creative, visual pattern using text, divs as shapes, images ... 
+   * 2: add in mouseover event listener(s) somewhere to make the sketch interactive
+   *
+   * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
+   * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
+   * this is so that your styles are not overriden by other teams.
+   * NOTE::: All your code is to be added here inside this function -
+   * remember you can define other functions inside....
+   * Do not change any code above or the HTML markup.
+   * **/
+
+  function aniB(parentCanvas) {
+    console.log("in ani-B -teamF");
+
+    // keep it inside the canvas
+    parentCanvas.style.position = "relative";
+    parentCanvas.style.overflow = "hidden";
+
+    parentCanvas.addEventListener("mousemove", paintRainbow);
+
+    function paintRainbow(e) {
+      let canvas = this.getBoundingClientRect();
+      let x = e.clientX - canvas.left;
+      let y = e.clientY - canvas.top;
+
+      // create dot
+      let dot = document.createElement("div");
+      dot.classList.add("TEAM_F_paintDot");
+      dot.textContent = "🙂";
+
+      // dot position
+      dot.style.left = x + "px";
+      dot.style.top = y + "px";
+
+      // randomize colors
+      let r = Math.floor(Math.random() * 256);
+      let g = Math.floor(Math.random() * 256);
+      let b = Math.floor(Math.random() * 256);
+      dot.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+
+      this.appendChild(dot);
+
+      setTimeout(() => {
+        dot.style.opacity = "0";
+      }, 10);
+
+      setTimeout(() => {
+        dot.remove();
+      }, 1000);
+    }
+  }
+  /****************ANI C ************************************ */
+  /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN C INSIDE HERE */
+  /****************ANI C************************************ */
+  /**************** TASK *******************************************
+   * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:)
+   * 1: use the PROVIDED keyup/down callbacks `windowKeyDownRef` and/or `windowKeyUpnRef` to handle keyboard events
+   * 2: create an interactive pattern/sketch based on keyboard input. Anything goes.
+   * 
+   * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
+   * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
+   * this is so that your styles are not overriden by other teams.
+   * NOTE::: All your code is to be added here inside this function -
+   * remember you can define other functions inside....
+   * Do not change any code above or the HTML markup.
+   * **/
+
+  /* TASK: make an interactive pattern .. colors, shapes, sizes, text, images....
+   * using  ONLY key down and/or keyup -- any keys::
+   */
+
+  function aniC(parentCanvas) {
+    console.log("in ani-C -teamF");
+    parentCanvas.style.overflow = "hidden";
+    parentCanvas.style.background = "linear-gradient(to bottom, #1b0ce8 0%, #463bea 50%, #6157ec 70%, #ecdd7b 100%)";
+    let sunY = 340;
+    let sun = document.createElement('div');
+    sun.classList.add('TEAM_F_sun');
+    parentCanvas.appendChild(sun);
+    function updateSunPosition() {
+      sun.style.top = sunY + "px";
+    }
+    updateSunPosition();
+
+    /*** THIS IS THE CALLBACK FOR KEY DOWN (* DO NOT CHANGE THE NAME *..) */
+    windowKeyDownRef = function (e) {
+      //code for key down in here
+      console.log(e);
+      console.log("f-down");
+      if (e.code === 'Space') {
+        console.log('Space is pressed');
+        e.preventDefault();
+        if (sunY >= 40) {
+          sunY = sunY - 20;
+          updateSunPosition();
+        };
+      } else if (e.code === 'Backspace') {
+        e.preventDefault();
+        if (sunY <= 280) {
+          sunY = sunY + 20;
+          updateSunPosition();
+        };
+      }
+    };
+
+    /*** THIS IS THE CALLBACK FOR KEY UP (*DO NOT CHANGE THE NAME..) */
+    windowKeyUpRef = function (e) {
+      console.log(e);
+      console.log("f-up");
+    };
+    //DO NOT REMOVE
+    window.addEventListener("keydown", windowKeyDownRef);
+    window.addEventListener("keyup", windowKeyUpRef);
+  }
+
+  /****************ANI D************************************ */
+  /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN D INSIDE HERE */
+  /****************ANI D************************************ */
+  /**************** TASK *******************************************
+   * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:).
+   * 1: create a creative, visual pattern using text, divs as shapes, images ...
+   * 2: add in animation using requestAnimationFrame somewhere to make the sketch animate :)
+   *
+   * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
+   * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
+   * this is so that your styles are not overriden by other teams.
+   * NOTE::: All your code is to be added here inside this function -
+   * remember you can define other functions inside....
+   * Do not change any code above or the HTML markup.
+   * **/
+  function aniD(parentCanvas) {
+    console.log("in ani-D -teamF");
+  }
+}
